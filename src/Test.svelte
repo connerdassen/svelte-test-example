f5b0aa448f2825ef63d4a990673df0bc37ba659d
Conner Dassen <connerdassen@gmail.com>
2023-06-11 21:37:06 +0200

    update

--- src/Test.svelte
+++ src/Test.svelte
@@ -1,9 +1,9 @@
 <script>
-    import { onMount } from "svelte";
+    import { onMount } from "svelte"; // onMount will not be called 
+    // import { onMount } from "svelte/internal"; // onMount will be called 
 
-    onMount(() => console.log("This does not"))
+    onMount(() => console.log("onMount was called"))
 
-    console.log("This prints");
 </script>
 
 <span>Hello world!</span>
